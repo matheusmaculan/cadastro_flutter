7007a15f02f7ab3101d1cf237e957c40a7832bbd
Matheus Maculan <matheus.maculan@hotmail.com>
2021-10-15 22:51:07 -0300

    Initial commit

---
+++ android/app/src/main/java/com/example/cadastro_flutter/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.cadastro_flutter;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Text('Bem vindo ao cadastro de Clientes!',textDirection: TextDirection.ltr));
+    // runAPP é basicamente o que irá rodar
