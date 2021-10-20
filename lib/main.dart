bcb4b867850745e5343793ff37b6ce9dcccd2297
matheus.maculan <matheus.maculan@hotmail.com>
2021-10-20 15:04:04 -0300

    Adicionado alguns conponentes

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,25 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
+          body: Column(
+            children: [
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.monetization_on),
+                  title: Text('Teste'),
+                  subtitle: Text('Subtítulo'),
+                )
+              ),              Card(
+                child: ListTile(
+                  leading: Icon(Icons.monetization_on),
+                  title: Text('2 Teste'),
+                  subtitle: Text('2 Subtítulo'),
+                )
+              ),
+            ],
+          ),
           appBar: AppBar(
-            title: Center(child: Text('Cadastro de Clientes', textAlign: TextAlign.center)),
+            title: Center(child: Text('Transferências', textAlign: TextAlign.center)),
           ),
         floatingActionButton: FloatingActionButton(
         onPressed: () {  },
