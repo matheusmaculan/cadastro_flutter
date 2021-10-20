3ce0713334f935439e6bfeca47cd03506ab9706c
matheus.maculan <matheus.maculan@hotmail.com>
2021-10-20 17:46:03 -0300

    CRiado a lógica para pegar os campos digitados e criar uma transferencia

--- lib/FormularioTransferencia.dart
+++ lib/FormularioTransferencia.dart
@@ -1,15 +1,60 @@
-
-
+import 'package:cadastro_flutter/Transferencia.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class FormularioTransferencia extends StatelessWidget {
+  final TextEditingController _controladorCampoNumeroConta =
+      TextEditingController();
+  final TextEditingController _controladorCampoValor = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(title: Text('Criando Transferencia'),),
-        body: Text('Teste'));
-
+        appBar: AppBar(
+          title: Text('Criando Transferência'),
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: _controladorCampoNumeroConta,
+                style: TextStyle(fontSize: 24.0),
+                decoration: InputDecoration(
+                  labelText: 'Número da Conta',
+                  hintText: '0000',
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: _controladorCampoValor,
+                style: TextStyle(fontSize: 24.0),
+                decoration: InputDecoration(
+                    icon: Icon(Icons.monetization_on),
+                    labelText: 'Valor da transferencia',
+                    hintText: '0.00'),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            ElevatedButton(
+              child: Text('Confirmar'),
+              onPressed: () {
+                debugPrint('Clicou no confirmar');
+                final int? numeroConta =
+                    int.tryParse(_controladorCampoNumeroConta.text);
+                final double? valor =
+                    double.tryParse(_controladorCampoValor.text);
+                if (numeroConta != null && valor != null) {
+                  final transferenciaCriada = Transferencia(valor, numeroConta);
+                  debugPrint('$transferenciaCriada');
+                } //endif
+              },
+            )
+          ],
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,5 +4,3 @@ import 'ByteBankApp.dart';
 
 void main() => runApp(ByteBankApp());
 
-
-// Hot Reload, a cada interação já é feito um refresh, sem precisar dar um restart na aplicação
